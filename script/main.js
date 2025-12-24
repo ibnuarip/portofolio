dd8c2c0d9b5c6ce896c9d253bc8c5328395cfec2
ibnuarip <ibnuarip1601@gmail.com>
2025-12-24 17:02:01 +0700

    update responsive

--- script/main.js
+++ script/main.js
@@ -1,19 +1,19 @@
 // Toggle & Responsive Navigation
 const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const navLists = document.querySelector("nav");
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector("nav");
 
-    burger.addEventListener("click", () => {
-        navLists.classList.toggle("nav-active");
-        burger.classList.toggle("toggle-burger");
-    });
+  burger.addEventListener("click", () => {
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
 };
 
 navSlide();
 
-// Clear form before unload 
+// Clear form before unload
 window.onbeforeunload = () => {
-    for (const form of document.getElementsByTagName("form")) {
-        form.reset();
-    }
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
 };
